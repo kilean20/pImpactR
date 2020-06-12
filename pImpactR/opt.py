a8699f36e06f087b1202cad047a998a2b01298be
kileean20 <kilean@lbl.gov>
2020-06-12 04:19:26 -0700

    commit amend6

--- pImpactR/opt.py
+++ pImpactR/opt.py
@@ -761,7 +761,7 @@ class DifferentialEvolutionSolver(object):
         nit, warning_flag = 0, False
         status_message = _status_message['success']
         
-        print('popsize=',np.size(population, 0))
+        print('popsize=',np.size(self.population, 0))
 
         # The population may have just been initialized (all entries are
         # np.inf). If it has you have to calculate the initial energies.
